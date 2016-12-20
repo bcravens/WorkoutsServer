db5cc1f134eafa422d400f63634bce47a46e4fd0
Bradley Cravens <bradleycravens@gmail.com>
2016-12-20 15:56:05 -0500

    Create functionality for workouts

--- src/routes/workouts.js
+++ src/routes/workouts.js
@@ -1,17 +1,50 @@
 import express from 'express'
-import authenticate from '../middlewares/authenticate'
+import commonValidations from '../shared/validations/workouts'
+import isEmpty from 'lodash/isEmpty'
+
 import Workouts from '../models/workouts'
 
 let router = express.Router()
 
-router.post('/', authenticate, (req, res) => {
-  res.status(201).json({ success: true })
-})
+function validateInput(data, otherValidations) {
+  let { errors } = otherValidations(data)
+
+  return Workouts.query({
+    where: { name: data.name }
+  }).fetch().then(workout => {
+    if (workout) {
+      if (workout.get('name') === data.name) {
+        errors.username = 'You already have a workout with that name'
+      }
+    }
+    return {
+      errors,
+      isValid: isEmpty(errors)
+    }
+  })
+
+}
 
 router.get('/workouts', (req, res) => {
   Workouts.query().then( workouts => {
     res.json({ workouts })
   })
+})
+
+router.post('/', (req, res) => {
+  validateInput(req.body, commonValidations).then(({ errors, isValid }) => {
+    if (isValid) {
+      const { name, user_id } = req.body
+      Workouts.forge({
+        name, user_id
+      }, { hasTimestamps: true }).save()
+        .then(user => res.json({ success: true }))
+        .catch(err => res.status(500).json({ error: err }))
+    } else {
+      res.status(400).json(errors)
+    }
+  })
+
 })
 
 export default router
